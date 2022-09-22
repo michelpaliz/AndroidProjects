ef61ce7d3a30c41ab5792f91cc1427c2e20ff205
michael <michelpaliz@hotmail.com>
2022-09-22 14:09:48 +0200

    changes

--- guess/app/src/main/java/com/example/guess/MainActivity.java
+++ guess/app/src/main/java/com/example/guess/MainActivity.java
@@ -4,113 +4,104 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = "MyActivity";
-    public static final int MAX_FALLOS = 3;
-
-    Button button1, button2, button3, button4, button5, button6, button7, button8, button9, button10;
-    // Button button1;
+    private static final int  MAX_FALLOS = 3;
+    private static int intentos = 0;
+    private static int luckyNumber = Lib.Util.randBetween(1, 10);;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Creamos la app
-        // button1.findViewById(R.id.button1);
-        // button1 = (Button) findViewById(R.id.button1);
-        // button1.setOnClickListener(new View.OnClickListener() {
-        // @Override
-        // public void onClick(View view) {
-        // Toast.makeText(getApplicationContext(), "Desde aqui",
-        // Toast.LENGTH_LONG).show();
-        // }
-        // });
     }
 
-    // Vamos a crear primero los numero aleatorios;
+    public void trampa(View view){
+        TextView tvLuckyNumber = findViewById(R.id.luckyNumber);
+        tvLuckyNumber.setText(String.valueOf(luckyNumber));
+        if(view.getId() == R.id.button){
+            Toast.makeText(this, "EL numero secreto es " + luckyNumber, Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    public void game(int eleccion) {
+        if (eleccion != luckyNumber && intentos < MAX_FALLOS) {
+            ++intentos;
+            Toast.makeText(getApplicationContext(), "Intentalo otra vez", Toast.LENGTH_SHORT).show();
+        }else if(eleccion != luckyNumber&& intentos == MAX_FALLOS){
+            Toast.makeText(getApplicationContext(), "Perdistes", Toast.LENGTH_SHORT).show();
+            finishAffinity();
+        }else if(eleccion == luckyNumber && intentos < MAX_FALLOS) {
+            Toast.makeText(getApplicationContext(), "Ganastes", Toast.LENGTH_LONG).show();
+            finishAffinity();
+        }
+    }
+
+
+    public void onClick2(View view) {
+        trampa(view);
+    }
+
 
     @Override
     public void onClick(View view) {
-        int luckyNumber = Lib.Util.randBetween(1, 10);
-        TextView tvLuckyNumber = findViewById(R.id.luckyNumber);
-        tvLuckyNumber.setText(String.valueOf(luckyNumber));
 
+        TextView informacion = (TextView) findViewById(R.id.informacion);
+        informacion.setText("numero de intentos " + intentos);
 
+//        Log.d("luckyNumber es ","" + luckyNumber);
+//        System.out.println("luckynumber" + luckyNumber);
         int eleccion = 0;
-        int contador = 0;
-        boolean correct = false;
-        boolean pierde = false;
-        do {
-            // APP
-
-            switch (view.getId()) {
-                case R.id.button1:
-                    eleccion = 1;
-                    break;
-                case R.id.button2:
-                    eleccion = 2;
-                    break;
-                case R.id.button3:
-                    eleccion = 3;
-                    break;
-                case R.id.button4:
-                    eleccion = 4;
-                    break;
-                case R.id.button5:
-                    eleccion = 5;
-                    break;
-                case R.id.button6:
-                    eleccion = 6;
-                    break;
-                case R.id.button7:
-                    eleccion = 7;
-                    break;
-                case R.id.button8:
-                    eleccion = 8;
-                    break;
-                case R.id.button9:
-                    eleccion = 9;
-                    break;
-                case R.id.button10:
-                    eleccion = 10;
-                    break;
-            }
-            // si el usuario sobrepasa las oportunidades pierde la partida
-
-            if (contador == MAX_FALLOS) {
-                 Toast.makeText(getApplicationContext(), "Has superado los intentospermitidos", Toast.LENGTH_LONG)
-                 .show();
-                pierde = true;
-                correct = true;
-            }
-
-            // si falla el usuario el contador aumenta
-            if (luckyNumber != eleccion) {
-                contador++;
-                 Toast.makeText(getApplicationContext(), "Numero incorrecto intentalo denuevo", Toast.LENGTH_LONG)
-                 .show();
-                correct = false;
-            } else {
-                // has ganado
-                 Toast.makeText(getApplicationContext(), "Ganastes",
-                 Toast.LENGTH_LONG).show();
-                correct = true;
-            }
-
-        } while (!correct);
-
-        if (pierde == true) {
-             Toast.makeText(getApplicationContext(), "Perdistes",
-             Toast.LENGTH_LONG).show();
+        switch (view.getId()) {
+            case R.id.button1:
+                eleccion = 1;
+                break;
+            case R.id.button2:
+                eleccion = 2;
+                break;
+            case R.id.button3:
+                eleccion = 3;
+                break;
+            case R.id.button4:
+                eleccion = 4;
+                break;
+            case R.id.button5:
+                eleccion = 5;
+                break;
+            case R.id.button6:
+                eleccion = 6;
+                break;
+            case R.id.button7:
+                eleccion = 7;
+                break;
+            case R.id.button8:
+                eleccion = 8;
+                break;
+            case R.id.button9:
+                eleccion = 9;
+                break;
+            case R.id.button10:
+                eleccion = 10;
+                break;
         }
 
+//        Log.d("Eleccion es", "" + eleccion);
+//        System.out.println("eleccion" + eleccion);
+        game(eleccion);
+
     }
+
+
 
 }
