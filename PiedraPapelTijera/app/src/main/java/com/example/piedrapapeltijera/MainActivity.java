6f30e87eab64f239e6d7dd2d912b723d7ce861d4
michael <michelpaliz@hotmail.com>
2022-09-25 17:58:40 +0200

    changes

--- PiedraPapelTijera/app/src/main/java/com/example/piedrapapeltijera/MainActivity.java
+++ PiedraPapelTijera/app/src/main/java/com/example/piedrapapeltijera/MainActivity.java
@@ -5,19 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private final int MAX_PUNTOS = 3;
-    private int empate = 0;
+    private int empate;
     private int puntosUsuario = 0;
     private int puntosMaquina = 0;
     private Button btnPiedra;
     private Button btnPapel;
     private Button btnTijera;
     private TextView resultado;
+    private ImageView imgMaquina;
+    private ImageView imgUsuario;
     private int eleccion;
 
     @Override
@@ -27,6 +30,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnPiedra = (Button) findViewById(R.id.btnPiedra);
         btnPapel = (Button) findViewById(R.id.btnPapel);
         btnTijera = (Button) findViewById(R.id.btnTijera);
+        imgMaquina  =  (ImageView)  findViewById(R.id.imgMaquina);
+        imgUsuario  =  (ImageView)  findViewById(R.id.imgUsuario);
+        resultado = (TextView) findViewById(R.id.resultado);
 
 
     }
@@ -34,26 +40,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
 
-
         //Dar significado a las variables que el usuario escoje
         switch (view.getId()) {
             case R.id.btnPiedra:
+                imgUsuario.setImageResource(R.drawable.piedra);
                 eleccion = 1;
-//                eleccion = btnPiedra.getText().toString();
                 break;
             case R.id.btnPapel:
+                imgUsuario.setImageResource(R.drawable.papel);
                 eleccion = 2;
-//                eleccion = btnPapel.getText().toString();
                 break;
             case R.id.btnTijera:
+                imgUsuario.setImageResource(R.drawable.tijera);
                 eleccion = 3;
-//                eleccion = btnTijera.getText().toString();
                 break;
 
         }
 
         int resultadoMaquina = maquina();
         String combinacion = eleccion + "" + resultadoMaquina;
+
+        //mostramos los imagenes
+        if (resultadoMaquina == 1){
+            imgMaquina.setImageResource(R.drawable.piedra);
+        }else if(resultadoMaquina == 2){
+            imgMaquina.setImageResource(R.drawable.papel);
+        }else if(resultadoMaquina == 3){
+            imgMaquina.setImageResource(R.drawable.tijera);
+        }
+
+
 //        resultado.setText(combinacion);
         if (combinacion.equalsIgnoreCase("13") || combinacion.equalsIgnoreCase("31")) {
             if ((eleccion == 1 && resultadoMaquina == 1) || (eleccion == 3 && resultadoMaquina == 3)) {
@@ -83,11 +99,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         }
 
-        Toast.makeText(getApplicationContext(),"Empate " + empate + " Puntos usuario" + puntosUsuario + " " + "Puntos Maquina " + puntosMaquina, Toast.LENGTH_SHORT).show();
-//        resultado.setText("Puntos Usuario =" + puntosUsuario +" " + "Puntos Maquina" + puntosMaquina);
+        if(puntosUsuario == 3){
+            Toast.makeText(getApplicationContext(),"El usuario ha ganado", Toast.LENGTH_SHORT).show();
+            finishAffinity();
+        }else if(puntosMaquina == 3){
+            Toast.makeText(getApplicationContext(),"La maquina ha ganado", Toast.LENGTH_SHORT).show();
+            finishAffinity();
+        }
+
+        resultado.setText("Empates " + empate + " Puntos usuario " + puntosUsuario +" " + "Puntos Maquina " + puntosMaquina);
     }
+
+
+
     public int maquina() {
-        int resultado = Lib.Util.randBetween(0, 3);
+        int resultado = Lib.Util.randBetween(1, 3);
         return resultado;
     }
 
