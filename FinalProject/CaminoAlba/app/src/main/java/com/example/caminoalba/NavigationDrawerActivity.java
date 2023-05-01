884611af9cf593d6651b220529db31e0a9fb8a0f
michelpaliz <michelpaliz@hotmail.com>
2023-05-01 22:25:27 +0200

    Making some changes for the toolbar layout also adding the settings fragment for our navigation drawer

--- FinalProject/CaminoAlba/app/src/main/java/com/example/caminoalba/NavigationDrawerActivity.java
+++ FinalProject/CaminoAlba/app/src/main/java/com/example/caminoalba/NavigationDrawerActivity.java
@@ -2,31 +2,23 @@ package com.example.caminoalba;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import androidx.preference.PreferenceManager;
-
-import com.example.caminoalba.databinding.ActivityNavigationDrawerBinding;
 import com.example.caminoalba.models.Profile;
-import com.example.caminoalba.models.User;
 import com.google.android.material.navigation.NavigationView;
 import com.google.gson.Gson;
 import com.squareup.picasso.Picasso;
-
-import java.util.Locale;
-import java.util.Objects;
-
 
 public class NavigationDrawerActivity extends AppCompatActivity {
     private AppBarConfiguration mAppBarConfiguration;
@@ -34,23 +26,19 @@ public class NavigationDrawerActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation_drawer);
 
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
         Gson gson = new Gson();
+
         // ------ We get the user sent by the login activity -------
         String userStr = preferences.getString("profile", "");
         Profile profile = gson.fromJson(userStr, Profile.class);
 
-        com.example.caminoalba.databinding.ActivityNavigationDrawerBinding binding = ActivityNavigationDrawerBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setSupportActionBar(findViewById(R.id.toolbar));
 
-//        setSupportActionBar(binding.appBarNavigationDrawer.toolbar);
-        setSupportActionBar(binding.appBarNavigationDrawer.toolbar);
-//        binding.appBarNavigationDrawer.fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                .setAction("Action", null).show());
-        DrawerLayout drawer = binding.drawerLayout;
-        NavigationView navigationView = binding.navView;
-
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         View headerView = navigationView.getHeaderView(0);
         TextView tvName = headerView.findViewById(R.id.tvName);
         TextView tvEmail = headerView.findViewById(R.id.tvEmail);
@@ -63,40 +51,41 @@ public class NavigationDrawerActivity extends AppCompatActivity {
             Picasso.get().load(R.drawable.default_image).into(imgProfile);
         }
 
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+        //TO HIDE THE DEFAULT ACTION BAR NAME
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            ((ActionBar) actionBar).setDisplayShowTitleEnabled(false);
+        }
+
+        //TO PUT A COSTUME NAME FOR MY ACTION BAR
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        TextView titleTextView = toolbar.findViewById(R.id.toolbar_title);
+        titleTextView.setText("CAMINO  DEL  ALBA");
+
         if (profile.getUser().getType().equalsIgnoreCase("admin")){
             mAppBarConfiguration = new AppBarConfiguration.Builder(
-                    R.id.blogFragment, R.id.newsFragment, R.id.profileFragment, R.id.fragmentConfirmationPartnership, R.id.logOutFragment)
+                    R.id.blogFragment, R.id.newsFragment, R.id.profileFragment, R.id.fragmentConfirmationPartnership, R.id.logOutFragment, R.id.fragment_settings)
                     .setOpenableLayout(drawer)
                     .build();
-
             navigationView.getMenu().findItem(R.id.fragmentConfirmationPartnership).setVisible(true);
         }else{
             mAppBarConfiguration = new AppBarConfiguration.Builder(
-                    R.id.blogFragment, R.id.newsFragment, R.id.profileFragment, R.id.logOutFragment)
+                    R.id.blogFragment, R.id.newsFragment, R.id.profileFragment, R.id.logOutFragment, R.id.fragment_settings)
                     .setOpenableLayout(drawer)
                     .build();
-
             navigationView.getMenu().findItem(R.id.fragmentConfirmationPartnership).setVisible(false);
         }
+
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_navigation_drawer);
+
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
 
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.navigation_drawer, menu);
-        return true;
-    }
-
-    @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_navigation_drawer);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration) || super.onSupportNavigateUp();
     }
 }
